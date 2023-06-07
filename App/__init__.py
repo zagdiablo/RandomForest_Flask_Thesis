1c6ad256cfb77f833eda45a77429cef686d9fd35
Adhitia Nugraha <adhitia.nugrahatkj@gmail.com>
2023-06-07 11:50:12 +0700

    selesai buat kerangaka webapp

--- App/__init__.py
+++ App/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask_restful import Api, Resource
+from flask_wtf import CSRFProtect
 
 
 app = Flask(__name__)
+api = Api(app)
+csrf = CSRFProtect()
 
 
 def start_app():
     # applicatio config
     app.config["DEBUG"] = True
+    app.config["SECRET_KEY"] = "katakakekukukukakikakakukakukenapaku6969"
+
+    # cross site request forgery protection
+    csrf.init_app(app)
 
     # blueprints import
     from .public_views import public_views
--- App/public_views.py
+++ App/public_views.py
@@ -4,11 +4,53 @@ from flask import Blueprint, render_template, redirect, request
 public_views = Blueprint("public_views", __name__)
 
 
+#
+#
+# API untuk handle inisialisasi keperluan webapp
 @public_views.route("/", methods=["GET"])
 def init():
     return redirect("/home")
 
 
+#
+#
+# API untuk handle halaman home
 @public_views.route("/home", methods=["GET"])
 def home_page():
     return render_template("public/home.html")
+
+
+#
+#
+# API untuk handle halaman pencarian rumah
+@public_views.route("/cari_rumah", methods=["GET"])
+def cari_rumah_page():
+    return render_template("public/cari_rumah.html")
+
+
+@public_views.route("/get_price_suggestion", methods=["POST"])
+def get_price_suggestion():
+    #####
+    # TODO handle logika machine learning & query data hasil
+    lantai = request.form.get("lantai")
+    ukuran = request.form.get("ukuran")
+    print(lantai, ukuran)
+    #####
+
+    return redirect("/cari_rumah")
+
+
+#
+#
+# API untuk handle halaman tentang kami
+@public_views.route("/about", methods=["GET"])
+def about_page():
+    return render_template("public/about.html")
+
+
+#
+#
+# API untuk handle halaman kontak
+@public_views.route("/contact", methods=["GET"])
+def contact_page():
+    return render_template("public/contact.html")
